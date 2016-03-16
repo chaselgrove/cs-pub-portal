c48dd3b9ee88a296b31225d9ee27a80395f2a925
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2016-03-16 14:48:50 +0000

    restructured web app to (better) allow static content added stylesheet

--- app/app.py
+++ app/app.py
@@ -1,20 +1,26 @@
 import flask
 import pub
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
+    return flask.render_template('index.tmpl', root=flask.request.script_root)
+
+@app.route('/pmc')
+@app.route('/pmc/')
+def pmc_redir():
     pmc_id = flask.request.args.get('pmc_id')
     if not pmc_id:
-        return flask.render_template('index.tmpl', 
-                                     target=flask.request.script_root)
+        return flask.redirect(flask.url_for('index'))
     return flask.redirect(flask.url_for('publication', pmc_id=pmc_id))
 
-@app.route('/<pmc_id>')
+@app.route('/pmc/<pmc_id>')
 def publication(pmc_id):
     publication = pub.Publication(pmc_id)
-    return flask.render_template('pub.tmpl', pub=publication)
+    return flask.render_template('pub.tmpl', 
+                                 root=flask.request.script_root, 
+                                 pub=publication)
 
 if __name__ == '__main__':
     app.run(debug=True)
